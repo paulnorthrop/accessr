c2f45baef48632eaf430d95da828a5c856121819
Paul Northrop <p.northrop@ucl.ac.uk>
2021-09-24 14:20:44 +0100

    Initial commit

---
+++ R/accessr.R
@@ -0,0 +1,18 @@
+#' accessr: Produce accessible PDF documents using R markdown
+#'
+#' Accessible PDF documents are produced by creating Word documents from R
+#' markdown files and then PDF documents from these Word documents.  The first
+#' step uses the \code{\link[rmarkdown]{render}} function from the
+#' \href{https://cran.r-project.org/package=rmarkdown}{rmarkdown package}
+#' The second step uses
+#' \href{https://github.com/cognidox/OfficeToPDF}{OfficetoPDF}.
+#'
+#' @details See \code{\link{rmd2pdf}}.
+#' @references JJ Allaire, Yihui Xie, Jonathan McPherson, Javier Luraschi,
+#'   Kevin Ushey, Aron Atkins, Hadley Wickham, Joe Cheng, Winston Chang and
+#'   Richard Iannone (2021). rmarkdown: Dynamic Documents for R. R package
+#'   version 2.9. \url{https://rmarkdown.rstudio.com}.
+#' @seealso \code{\link{rmd2pdf}}.
+#' @docType package
+#' @name accessr
+NULL
---
+++ R/rmd2word2pdf.R
@@ -0,0 +1,94 @@
+# ================================ rmd2pdf ====================================
+
+#' Converts rmarkdown code to Word and PDF documents
+#'
+#' Creates accessible PDF documents by creating Word documents using
+#' rmarkdown code and then producing PDF files.
+#'
+#' @param x A logical vector containing the names of the files to convert
+#'   (no extension).
+#' @param dir The directory in which the file officetopdf.exe sits. See
+#'   \strong{Details}.
+#' @param doc The name of a template Word file on which to base the output.
+#'         If this is not in the working directory then also give the path.
+#' @param ... Additional arguments to be passed to \code{\link{system}}.
+#'   The argument \code{wait} determines whether or not R will wait for the
+#'   PDF files to be produced.  In the default \code{wait = TRUE} case a
+#'   warning will be given if any of the PDF files could not be produced.
+#'   This will occur if there is an existing PDF file of the same name open
+#'   in another application.  If \code{wait = FALSE} then no warnings will be
+#'   produced.
+#' @details The \code{\link[rmarkdown]{render}} function is used to create
+#'   a Word file in the current working directory.
+#'
+#'   Then
+#'   \href{https://github.com/cognidox/OfficeToPDF}{OfficeToPDF} is used to
+#'   convert the Word file to a PDF file.  The file \code{officetopdf.exe}
+#'   needs to be downloaded from the
+#'   \href{https://github.com/cognidox/OfficeToPDF/releases}{OfficeToPDF releases}
+#'   page and placed in the directory specified by the argument \code{dir}, or
+#'   in a directory that is on the list returned by \code{\link{searchpaths}}.
+#'   If \code{officetopdf.exe} cannot be found then an error is thrown.
+#'
+#'   \code{pns} and \code{pnd} are convenience functions for useonly by the
+#'   package maintainer.
+#' @return A vector containing the values returned from \code{\link{system}}
+#'   is returned invisibly. Note that if \code{wait = FALSE} then these values
+#'   will be 0 (the success value) even if some of the PDF files could not be
+#'   produced.  The error code 17234 indicates that a PDF file was open in
+#'   another application.
+#' @examples
+#' \dontrun{
+#'   rmd2pdf(c("file1", "file2"), dir = "C:/Users/paul/Documents/R_PACKAGES/",
+#'           doc = "template.docx")
+#' }
+#' @name rmd2pdf
+#' @export
+rmd2pdf <- function(x, dir = NULL, doc = NULL, ...) {
+  # Just in case there are repeated file names
+  x <- unique(x)
+  # Path to the officetopdf executable
+  if (is.null(dir)) {
+    exefile <- "officetopdf.exe"
+  } else {
+    exefile <- paste0(dir, "officetopdf.exe")
+  }
+  # If no template word document has been supplied then use the default
+  if (is.null(doc)) {
+    doc <- "default"
+  }
+  # Function for Rmd to Word to PDF
+  fun <- function(x) {
+    # Convert .Rmd file to Word document
+    rmarkdown::render(paste0(x, ".Rmd"),
+                      rmarkdown::word_document(reference_docx = doc))
+    # Convert Word document to PDF document
+    system(paste(exefile, paste0(x, ".docx"), paste0(x, ".pdf")), ...)
+  }
+  res <- sapply(x, fun)
+  # Error codes
+  # 127 officetopdf.exe could not be found
+  # 17234 file open in another application
+  if (any(res == 127)) {
+    stop("officetopdf.exe could not be found")
+  }
+  if (any(res) != 0) {
+    which(res != 0)
+    x[res != 0]
+    warning(paste0(x[res != 0], ".pdf "), "could not be written")
+  }
+  invisible(res)
+}
+
+#' @name rmd2pdf
+#' @export
+pns <- function(x, ...) {
+  rmd2pdf(x, dir = "C:/Users/Paul/", doc = "template.docx", ...)
+}
+
+#' @name rmd2pdf
+#' @export
+pnd <- function(x, ...) {
+  rmd2pdf(x, dir = "C:/Users/paul/Documents/R_PACKAGES/",
+      doc = "template.docx", ...)
+}
