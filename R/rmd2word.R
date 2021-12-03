a6cd2606d3389f90e76f97d4871c71ea0e5221b9
Paul Northrop <p.northrop@ucl.ac.uk>
2021-12-03 09:05:52 +0000

    Make accessr:: explicit

--- R/rmd2word.R
+++ R/rmd2word.R
@@ -173,7 +173,7 @@ rmd2word <- function(x, doc = "accessr", dir, zip = TRUE, add = FALSE,
                          x = plot_body)
     line_to_modify <- which(where_line)
     body(res0$knitr$knit_hooks$plot)[[line_to_modify]] <-
-      substitute(img <- ext_img(src = x[1], width = fig.width,
+      substitute(img <- accessr::ext_img(src = x[1], width = fig.width,
                                 height = fig.height, alt = options$fig.alt,
                                 ref_docx_dim = ref_docx_dim))
     # Render the Word file
