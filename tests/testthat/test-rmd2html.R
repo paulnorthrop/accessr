33be5a479d66d0132536b057effb210e6c6a1366
Paul Northrop <p.northrop@ucl.ac.uk>
2024-04-09 00:07:55 +0100

    Go back

--- tests/testthat/test-rmd2html.R
+++ tests/testthat/test-rmd2html.R
@@ -11,8 +11,6 @@ got_all <- got_hux && got_flex && got_pandoc
 # Check that rmd2html() returns the correct file names
 # Set pdf = FALSE to avoid using pagedown::chrome_print()
 
-got_all <- TRUE
-
 if (got_all) {
   # rmd2html()
   res <- rmd2html(ex_file, params = list(hide = TRUE), pdf = FALSE, zip = TRUE)
