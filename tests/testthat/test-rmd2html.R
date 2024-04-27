419835d7bf87f2499ee6ecc85e981cf1574be58b
Paul Northrop <p.northrop@ucl.ac.uk>
2024-04-27 21:19:53 +0100

    Now in setup.R

--- tests/testthat/test-rmd2html.R
+++ tests/testthat/test-rmd2html.R
@@ -1,13 +1,3 @@
-# Copy the example.Rmd file to be used in tests
-ex_file <- system.file(package = "accessr", "examples", "example.Rmd")
-ex_file <- sub(".Rmd", "", ex_file)
-
-# Check for packages needed for the tests
-got_hux <- requireNamespace("huxtable", quietly = TRUE)
-got_flex <- requireNamespace("flextable", quietly = TRUE)
-got_pandoc <- rmarkdown::pandoc_available("1.14")
-got_all <- got_hux && got_flex && got_pandoc
-
 # Check that rmd2html() returns the correct file names
 # Set pdf = FALSE to avoid using pagedown::chrome_print()
 
